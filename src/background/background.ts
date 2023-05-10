e7a3c768b8a8072abc446f6f27568bf6ccf89d36
Rishabh Rastogi <rishabh0211@gmail.com>
2023-05-10 18:38:44 +0530

    background script

---
+++ src/background/background.ts
@@ -0,0 +1,24 @@
+// background script for blocking ads when using manifest_version 2
+
+chrome.webRequest.onBeforeRequest.addListener((details) => {
+  const { url } = details;
+  // '*://*.googlesyndication.com/*',
+  // '*://googleads.g.doubleclick.net/*',
+  // '*://adclick.g.doubleclick.net/*',
+  const filters = [
+    'googleadservice',
+    'googlesyndication',
+    'g.doubleclick'
+  ];
+  let cancel = false;
+  for (const filter of filters) {
+    if (url.includes(filter)) {
+      console.log('cancelling : ', url);
+      cancel = true;
+      break;
+    }
+  }
+  return { cancel }
+}, {
+  urls: ['<all_urls>'],
+}, ['blocking']);
